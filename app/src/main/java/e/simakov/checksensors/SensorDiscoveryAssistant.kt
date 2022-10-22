26ff26535e23ed5de3e0a823fdc16d0e7fa2b18f
Evgeny.Simakov <Evgeny.Simakov@russianpost.ru>
2022-10-22 14:59:49 +0300

    1. Removed double methods.

--- app/src/main/java/e/simakov/checksensors/SensorDiscoveryAssistant.kt
+++ app/src/main/java/e/simakov/checksensors/SensorDiscoveryAssistant.kt
@@ -1,7 +1,6 @@
 package e.simakov.checksensors
 
 import android.content.Context
-import android.hardware.Sensor
 import android.hardware.Sensor.*
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
@@ -9,194 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 abstract class SensorDiscoveryAssistant : AppCompatActivity() {
 
     private lateinit var sensorManager: SensorManager
-    lateinit var magnetometerMessage: String
-    lateinit var accelerometerMessage: String
-    lateinit var ambientTemperatureMessage: String
-    lateinit var gravityMessage: String
-    lateinit var gyroscopeMessage: String
-    lateinit var lightMessage: String
-    lateinit var linearAcceleratorMessage: String
-    lateinit var magneticFieldMessage: String
-    lateinit var typeOrientationMessage: String
-    lateinit var pressureMessage: String
-    lateinit var proximityMessage: String
-    lateinit var relativeHumidityMessage: String
-    lateinit var rotationVectorMessage: String
-    lateinit var temperatureMessage: String
+    lateinit var sensorMessage: String
 
-
-    companion object {
-        const val TITLE_MAGNETOMETER = "Магнетометр"
-        const val TITLE_ACCELEROMETER = "Акселерометр"
-        const val TITLE_AMBIENT_TEMPERATURE = "Датчик внешней температуры"
-        const val TITLE_GRAVITY = "Датчик гравитации"
-        const val TITLE_GYROSCOPE = "Гироскоп"
-        const val TITLE_LIGHT = "Датчик света"
-        const val TITLE_LINEAR_ACCELERATOR = "Линейный акселератор"
-        const val TITLE_MAGNETIC_FIELD = "Датчик магнитного поля"
-        const val TITLE_TYPE_ORIENTATION = "Датчик ориентации"
-        const val TITLE_PRESSURE = "Датчик давления"
-        const val TITLE_PROXIMITY = "Датчик приближения"
-        const val TITLE_RELATIVE_HUMIDITY = "Датчик влажности"
-        const val TITLE_ROTATION_VECTOR = "Датчик направления вращения"
-        const val TITLE_TEMPERATURE = "Датчик температуры"
-        const val TEXT_SENSOR_THERE = "Есть"
-        const val TEXT_SENSOR_NOT_THERE = "Отсутствует"
-
-    }
-
-    fun checkMagnetometer(): String {
+    fun checkSensors(nameSensors: Int, title:String): String {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_MAGNETIC_FIELD) != null) {
-            magnetometerMessage = TEXT_SENSOR_THERE
-            return "$TITLE_MAGNETOMETER"
+        return if (sensorManager.getDefaultSensor(nameSensors) != null) {
+            sensorMessage = "Есть"
+            "$title"
         } else {
-            magnetometerMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_MAGNETOMETER"
-        }
-    }
-
-    fun checkAccelerometer(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_ACCELEROMETER) != null) {
-            accelerometerMessage = TEXT_SENSOR_THERE
-            return "$TITLE_ACCELEROMETER"
-
-        } else {
-            accelerometerMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_ACCELEROMETER"
-        }
-    }
-
-    fun checkAmbientTemperature(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_AMBIENT_TEMPERATURE) != null) {
-            ambientTemperatureMessage = TEXT_SENSOR_THERE
-            return "$TITLE_AMBIENT_TEMPERATURE"
-        } else {
-            ambientTemperatureMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_AMBIENT_TEMPERATURE"
-        }
-    }
-
-    fun checkGravity(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_GRAVITY) != null) {
-            gravityMessage = TEXT_SENSOR_THERE
-            return "$TITLE_GRAVITY"
-        } else {
-            gravityMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_GRAVITY"
-        }
-    }
-
-    fun checkGyroscope(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_GYROSCOPE) != null) {
-            gyroscopeMessage = TEXT_SENSOR_THERE
-            return "$TITLE_GYROSCOPE"
-        } else {
-            gyroscopeMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_GYROSCOPE"
-        }
-    }
-
-    fun checkLight(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_LIGHT) != null) {
-            lightMessage = TEXT_SENSOR_THERE
-            return "$TITLE_LIGHT"
-        } else {
-            lightMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_LIGHT"
-        }
-    }
-
-    fun checkLinearAccelerator(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_LINEAR_ACCELERATION) != null) {
-            linearAcceleratorMessage = TEXT_SENSOR_THERE
-            return "$TITLE_LINEAR_ACCELERATOR"
-        } else {
-            linearAcceleratorMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_LINEAR_ACCELERATOR"
-        }
-    }
-
-    fun checkMagneticField(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_MAGNETIC_FIELD) != null) {
-            magneticFieldMessage = TEXT_SENSOR_THERE
-            return "$TITLE_MAGNETIC_FIELD"
-        } else {
-            magneticFieldMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_MAGNETIC_FIELD"
-        }
-    }
-
-    fun checkTypeOrientation(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_ORIENTATION) != null) {
-            typeOrientationMessage = TEXT_SENSOR_THERE
-            return "$TITLE_TYPE_ORIENTATION"
-        } else {
-            typeOrientationMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_TYPE_ORIENTATION"
-        }
-    }
-
-    fun checkPressure(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_PRESSURE) != null) {
-            pressureMessage = TEXT_SENSOR_THERE
-            return "$TITLE_PRESSURE"
-        } else {
-            pressureMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_PRESSURE"
-        }
-    }
-
-    fun checkProximity(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_PROXIMITY) != null) {
-            proximityMessage = TEXT_SENSOR_THERE
-            return "$TITLE_PROXIMITY"
-        } else {
-            proximityMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_PROXIMITY"
-        }
-    }
-
-    fun checkRelativeHumidity(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_RELATIVE_HUMIDITY) != null) {
-            relativeHumidityMessage = TEXT_SENSOR_THERE
-            return "$TITLE_RELATIVE_HUMIDITY"
-        } else {
-            relativeHumidityMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_RELATIVE_HUMIDITY"
-        }
-    }
-
-    fun checkRotationVector(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_ROTATION_VECTOR) != null) {
-            rotationVectorMessage = TEXT_SENSOR_THERE
-            return "$TITLE_ROTATION_VECTOR"
-        } else {
-            rotationVectorMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_ROTATION_VECTOR"
-        }
-    }
-
-    fun checkTemperature(): String {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        if (sensorManager.getDefaultSensor(TYPE_TEMPERATURE) != null) {
-            temperatureMessage = TEXT_SENSOR_THERE
-            return "$TITLE_TEMPERATURE"
-        } else {
-            temperatureMessage = TEXT_SENSOR_NOT_THERE
-            return "$TITLE_TEMPERATURE"
+            sensorMessage = "Отсутствует"
+            "$title"
         }
     }
 }
--- app/src/main/java/e/simakov/checksensors/SensorsActivity.kt
+++ app/src/main/java/e/simakov/checksensors/SensorsActivity.kt
@@ -1,9 +1,9 @@
 package e.simakov.checksensors
 
+import android.hardware.Sensor
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import e.simakov.checksensors.adapter.SensorAdapter
-import e.simakov.checksensors.databinding.ActivityMainBinding
 import e.simakov.checksensors.databinding.ActivitySensorBinding
 import e.simakov.checksensors.model.SensorModel
 
@@ -12,6 +12,41 @@ class SensorsActivity : SensorDiscoveryAssistant() {
     lateinit var binding: ActivitySensorBinding
     lateinit var adapter: SensorAdapter
     lateinit var recyclerView: RecyclerView
+
+
+    companion object {
+        const val TITLE_MAGNETOMETER = "Магнетометр"
+        const val TITLE_ACCELEROMETER = "Акселерометр"
+        const val TITLE_AMBIENT_TEMPERATURE = "Датчик внешней температуры"
+        const val TITLE_GRAVITY = "Датчик гравитации"
+        const val TITLE_GYROSCOPE = "Гироскоп"
+        const val TITLE_LIGHT = "Датчик света"
+        const val TITLE_LINEAR_ACCELERATOR = "Линейный акселератор"
+        const val TITLE_MAGNETIC_FIELD = "Датчик магнитного поля"
+        const val TITLE_TYPE_ORIENTATION = "Датчик ориентации"
+        const val TITLE_PRESSURE = "Датчик давления"
+        const val TITLE_PROXIMITY = "Датчик приближения"
+        const val TITLE_RELATIVE_HUMIDITY = "Датчик влажности"
+        const val TITLE_ROTATION_VECTOR = "Датчик направления вращения"
+        const val TITLE_TEMPERATURE = "Датчик температуры"
+    }
+
+
+
+var listSensors = mapOf("magnetometer" to Sensor.TYPE_MAGNETIC_FIELD,
+    "sensorAccelerometer" to Sensor.TYPE_ACCELEROMETER,
+    "sensorAmbientTemperature" to Sensor.TYPE_AMBIENT_TEMPERATURE,
+    "sensorGravity" to Sensor.TYPE_GRAVITY,
+    "sensorGyroscope" to Sensor.TYPE_GYROSCOPE,
+    "sensorLight" to Sensor.TYPE_LIGHT,
+    "sensorLinearAccelerator" to Sensor.TYPE_LINEAR_ACCELERATION,
+    "sensorMagneticField" to Sensor.TYPE_MAGNETIC_FIELD,
+    "sensorTypeOrientation" to Sensor.TYPE_ORIENTATION,
+    "sensorPressure" to Sensor.TYPE_PRESSURE,
+    "sensorProximity" to Sensor.TYPE_PROXIMITY,
+    "sensorRelativeHumidity" to Sensor.TYPE_RELATIVE_HUMIDITY,
+    "sensorRotationVector" to Sensor.TYPE_ROTATION_VECTOR,
+    "sensorTemperature" to Sensor.TYPE_TEMPERATURE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,20 +65,20 @@ class SensorsActivity : SensorDiscoveryAssistant() {
     private fun myUser(): ArrayList<SensorModel>{
         val userList = ArrayList<SensorModel>()
 
-        val sensorMagnetometer = SensorModel(checkMagnetometer(), magnetometerMessage)
-        val userAccelerometer = SensorModel(checkAccelerometer(), accelerometerMessage)
-        val userAmbientTemperature = SensorModel(checkAmbientTemperature(), ambientTemperatureMessage)
-        val userGravity = SensorModel(checkGravity(), gravityMessage)
-        val userGyroscope = SensorModel(checkGyroscope(), gyroscopeMessage)
-        val userLight = SensorModel(checkLight(), lightMessage)
-        val userLinearAccelerator = SensorModel(checkLinearAccelerator(), linearAcceleratorMessage)
-        val userMagneticField = SensorModel(checkMagneticField(), magneticFieldMessage)
-        val userTypeOrientation = SensorModel(checkTypeOrientation(), typeOrientationMessage)
-        val userPressure = SensorModel(checkPressure(), pressureMessage)
-        val userProximity = SensorModel(checkProximity(), proximityMessage)
-        val userRelativeHumidity = SensorModel(checkRelativeHumidity(), relativeHumidityMessage)
-        val userRotationVector = SensorModel(checkRotationVector(), rotationVectorMessage)
-        val userTemperature = SensorModel(checkTemperature(), temperatureMessage)
+        val sensorMagnetometer = SensorModel(checkSensors(listSensors.getValue("magnetometer"), TITLE_MAGNETOMETER), sensorMessage)
+        val userAccelerometer = SensorModel(checkSensors(listSensors.getValue("sensorAccelerometer"), TITLE_ACCELEROMETER), sensorMessage)
+        val userAmbientTemperature = SensorModel(checkSensors(listSensors.getValue("sensorAmbientTemperature"), TITLE_AMBIENT_TEMPERATURE), sensorMessage)
+        val userGravity = SensorModel(checkSensors(listSensors.getValue("sensorGravity"), TITLE_GRAVITY), sensorMessage)
+        val userGyroscope = SensorModel(checkSensors(listSensors.getValue("sensorGyroscope"), TITLE_GYROSCOPE), sensorMessage)
+        val userLight = SensorModel(checkSensors(listSensors.getValue("sensorLight"), TITLE_LIGHT), sensorMessage)
+        val userLinearAccelerator = SensorModel(checkSensors(listSensors.getValue("sensorLinearAccelerator"), TITLE_LINEAR_ACCELERATOR), sensorMessage)
+        val userMagneticField = SensorModel(checkSensors(listSensors.getValue("sensorMagneticField"), TITLE_MAGNETIC_FIELD), sensorMessage)
+        val userTypeOrientation = SensorModel(checkSensors(listSensors.getValue("sensorTypeOrientation"), TITLE_TYPE_ORIENTATION), sensorMessage)
+        val userPressure = SensorModel(checkSensors(listSensors.getValue("sensorPressure"), TITLE_PRESSURE), sensorMessage)
+        val userProximity = SensorModel(checkSensors(listSensors.getValue("sensorProximity"), TITLE_PROXIMITY), sensorMessage)
+        val userRelativeHumidity = SensorModel(checkSensors(listSensors.getValue("sensorRelativeHumidity"), TITLE_RELATIVE_HUMIDITY), sensorMessage)
+        val userRotationVector = SensorModel(checkSensors(listSensors.getValue("sensorRotationVector"), TITLE_ROTATION_VECTOR), sensorMessage)
+        val sensorTemperature = SensorModel(checkSensors(listSensors.getValue("sensorTemperature"), TITLE_TEMPERATURE), sensorMessage)
 
         userList.add(sensorMagnetometer)
         userList.add(userAccelerometer)
@@ -58,10 +93,8 @@ class SensorsActivity : SensorDiscoveryAssistant() {
         userList.add(userProximity)
         userList.add(userRelativeHumidity)
         userList.add(userRotationVector)
-        userList.add(userTemperature)
+        userList.add(sensorTemperature)
 
         return userList
-
     }
-
 }
